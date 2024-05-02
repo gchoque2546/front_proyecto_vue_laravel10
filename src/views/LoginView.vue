1574130a7807d358a77b41caa3790abc4d5de955
gabriel <gabovchoque@gmail.com>
2024-05-02 19:23:59 -0400

    Login Auth Vue Interceptores

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,19 +1,36 @@
 <script setup>
-import { ref } from "vue"
-import axios from "axios"
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter, useRoute } from "vue-router"
+import authService from "./../services/auth.service"
 
     //variables
-    const correo = ref ("juan@mail.com");
-    const clave = ref ("juan54321");
+    // const correo = ref ("juan@mail.com");
+    // const clave = ref ("juan54321");
 
     const usuario = ref({email:"", password:""})
+    const errors = ref({})
+
+    const router = useRouter()
     //metodos
-    function funIngresar() {
-        axios.post("http://127.0.0.1:8000/api/v1/auth/login", usuario.value).then(
+    async function funIngresar() {
+        try {
+            const { data } = await authService.loginConLaravel(usuario.value)
+            console.log("CON INTERCEPTOR", data)
+            errors.value = {}
+            localStorage.setItem("access_token", data.access_token);
+            router.push("/about")
+        } catch (error) {
+            console.log(error.response.data)
+            errors.value = error.response.data.errors
+        }
+        
+        
+        /*axios.post("http://127.0.0.1:8000/api/v1/auth/login", usuario.value).then(
             (res) => {
-                console.log(res)
+                console.log("CON AXIOS", res.data)
             }
-        )
+        )*/
     }
 </script>
 
@@ -23,10 +40,13 @@ import axios from "axios"
         <h1>Login</h1>
         <label for="e">Ingresar Correo:</label>
         <input type="email" v-model="usuario.email">
+        <span>{{ errors.email }}</span>
         <br>
         <label>Ingresar Contraseña:</label>
         <input type="password" v-model="usuario.password">
+        <span>{{ errors.password }}</span>
         <br>
         <input type="button" value="Ingresar" v-on:click="funIngresar">
+        
     </div>
 </template>
